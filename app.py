254c6032ad15b146139333b2faa6392515c7cb44
EmmanuelRCPP <emmanuel.rodriguez.cortez@gmail.com>
2025-02-22 11:42:44 -0800

    fdkjsdo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
-    return "<h1>adiosmundo</h1>"
+    return "<h1>WoppagangamStyle</h1>"
 
 if __name__ == "__main__":
     app.run()
